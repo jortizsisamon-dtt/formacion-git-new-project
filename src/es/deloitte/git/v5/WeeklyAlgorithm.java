21476faffe598dd6543cfefdea73ca738e055a58
Juan Manuel Ortiz Sisamón <jortizsisamon@deloitte.es>
2020-10-06 12:20:47 +0200

    Add new Java class versions 4 and 5
    Add new Java class versions with more examples

---
+++ src/es/deloitte/git/v5/WeeklyAlgorithm.java
@@ -0,0 +1,27 @@
+package es.deloitte.git.v2;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.stream.Collectors;
+
+public class WeeklyAlgorithm {
+
+    public static void main(String[] args) {
+        System.out.println("=== WEEKLY ALGORITHM I ===");
+        getOrderedListBySurname1WA();
+        System.out.println("==========================");
+    }
+
+    private static void getOrderedListBySurname1WA() {
+        System.out.println("For this String:");
+        String unorderedNamesList = "Martin Fowler;Ken Schwaber;Arie VanBennekum;Alistair Cockburn;Ward Cunningham;Kent Beck;Andrew Hunt;Brian Marick;Dave Thomas;Mike Beedle;Ron Jeffries;Jon Kern;James Grenning;Robert Martin;Steve Mellor;Jeff Schwaber;Jim Highsmith";
+        System.out.println(unorderedNamesList);
+        System.out.println("Order it by surname.");
+        System.out.println("RESPONSE: ");
+        String orderedNamesList = Arrays.stream(unorderedNamesList.split(";"))
+                .sorted(Comparator.comparing(a -> a.toString().split(" ")[1])
+                        .thenComparing(a -> a.toString().split(" ")[0]))
+                .collect(Collectors.joining(";"));
+        System.out.println(orderedNamesList);
+    }
+}
